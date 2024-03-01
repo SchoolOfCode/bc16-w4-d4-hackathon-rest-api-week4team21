c3216ba62bc81ebe2e17bfca99297626ae5532e1
1ravi48 <ravi48@hotmail.co.uk>
2024-03-01 10:51:21 +0000

    ticket 2 done

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
 const app = express();
 const PORT = 3000;
+
+const filePath = path.resolve(process.cwd(), "questions.json");
 
 app.use(express.json());
 
 app.get("/", function (req, res) {
 	res.send("Welcome to the questions API");
 });
+
+app.get("/questions", async function (req, res) {
+	const questionsJSON = await fs.readFile(filePath, "utf-8");
+	const questions = JSON.parse(questionsJSON);
+	res.json(questions)
+})
+
 
 app.listen(PORT, function () {
 	console.log(`Server is now listening on http://localhost:${PORT}`);
